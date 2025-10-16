e35d60f87af9ecca7a442bbe660673cb993c4d30
zaybournand <zaybournand@gmail.com>
2025-10-16 15:28:29 -0400

    Added authentication routes, report routes, and location route

---
+++ backend/models.py
@@ -0,0 +1,19 @@
+from mongoengine import Document, StringField, DateTimeField, IntField
+
+
+class User(Document):
+    username = StringField(required=True, unique=True, max_length=200)
+    password_hash = StringField(required=True)
+    email = StringField(required=True, unique=True)
+    is_admin = BooleanField(default=False)
+
+class Report(Document):
+    author = StringField(required=True, max_length=200)
+    location_name = StringField(max_length=200)
+    date_posted = DateTimeField()
+    busyness_level = IntField(min_value=1, max_value=10)
+
+
+class Location(Document):
+    name = StringField(required=True, unique=True, max_length=200)
+    current_busyness = IntField()
